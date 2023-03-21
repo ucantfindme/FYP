6b798088d16295bcd28717cf7186913919082377
Sai Pavan Krishna Nagam <58015892+ucantfindme@users.noreply.github.com>
2023-03-21 00:12:15 -0400

    Update settings.py

--- src/django/guessit/guessit/settings.py
+++ src/django/guessit/guessit/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^nq9l^s)f*o#rymmtog2se4d(esl+p5je*1fe-#$+j!jd_#kcu'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
