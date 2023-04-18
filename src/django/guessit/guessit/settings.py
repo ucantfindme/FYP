1dd5a541fedcfec4d60c7b000a45be0e46fae052
ucantfindme <8765s.p.krishna.n@gmail.com>
2023-04-18 04:49:28 -0400

    new

--- src/django/guessit/guessit/settings.py
+++ src/django/guessit/guessit/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '123'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://guess-it-fyp-test.herokuapp.com/','*','127.0.0.1']
 
 
 # Application definition
